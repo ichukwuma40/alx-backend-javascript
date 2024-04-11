1bd724feaf1ef6e586061db8e6fdb62713de0294
Innocent Chukwuma <ichukwuma40@gmail.com>
2024-04-11 13:14:59 -0700

    ES6_basic

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+	let arrayEnd = [];
+	for (const idx of array) {
+		arrayEnd.push(`${appendString}${idx}`);
+	}
+	return arrayEnd;
+}
